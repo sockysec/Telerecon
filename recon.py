2633a94494c08350c8eaaa27c8f14e421abb3764
sockysec <121141737+sockysec@users.noreply.github.com>
2023-09-01 12:45:34 +1200

    Update recon.py

--- recon.py
+++ recon.py
@@ -1,7 +1,7 @@
 import os
 import asyncio
 import details as ds
-from telethon import TelegramClient, errors, types
+from telethon import TelegramClient, errors
 import pandas as pd
 from tqdm import tqdm
 from colorama import Fore, Style
@@ -27,18 +27,18 @@ async def count_user_posts(channel_name, target_user):
             return post_count
 
         except (ValueError, errors.FloodWaitError) as ve:
-            print(f"{Fore.RED}Error: {ve}{Style.RESET_ALL}")
+            print(f"\n{Fore.RED}Error: {ve}{Style.RESET_ALL}")
             return 0
         except Exception as e:
-            print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
+            print(f"\n{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
             return 0
 
 async def main():
-    target_user = input("Please enter the target user's @username or User ID: ")
-    target_list_filename = input("Please enter the filename of the target channel list (csv/txt): ")
+    target_user = input(f"{Fore.CYAN}Please enter the target user's @username or User ID: {Style.RESET_ALL}")
+    target_list_filename = input(f"{Fore.CYAN}Please enter the filename of the target channel list (csv/txt): {Style.RESET_ALL}")
 
     if not os.path.exists(target_list_filename):
-        print(f"{Fore.RED}Error: The file '{target_list_filename}' does not exist.{Style.RESET_ALL}")
+        print(f"\n{Fore.RED}Error: The file '{target_list_filename}' does not exist.{Style.RESET_ALL}")
         return
 
     target_channels = []
@@ -46,25 +46,25 @@ async def main():
         target_channels = [line.strip() for line in file]
 
     if not target_channels:
-        print(f"{Fore.YELLOW}Warning: No target channels found in the input file.{Style.RESET_ALL}")
+        print(f"\n{Fore.YELLOW}Warning: No target channels found in the input file.{Style.RESET_ALL}")
         return
 
     post_counts = {}
 
     for target_channel in target_channels:
-        print(f"{Fore.CYAN}Checking activity in {target_channel}...{Style.RESET_ALL}")
+        print(f"\n{Fore.CYAN}Checking activity in {Fore.YELLOW}{target_channel}...{Style.RESET_ALL}")
         post_count = await count_user_posts(target_channel, target_user)
         post_counts[target_channel] = post_count
 
         if post_count > 0:
-            print(f"{Fore.GREEN}{target_user} has {post_count} posts in {target_channel}.{Style.RESET_ALL}")
+            print(f"\n{Fore.CYAN}{target_user} has {Fore.GREEN}{post_count}{Fore.CYAN} posts in {Fore.YELLOW}{target_channel}.{Style.RESET_ALL}")
         else:
-            print(f"{Fore.YELLOW}{target_user} has no posts in {target_channel}.{Style.RESET_ALL}")
+            print(f"\n{Fore.CYAN}{target_user} has {Fore.RED}no posts{Fore.CYAN} in {Fore.YELLOW}{target_channel}.{Style.RESET_ALL}")
 
     csv_filename = f'{target_user}_post_counts.csv'
     df = pd.DataFrame(post_counts.items(), columns=['Channel', 'Post Count'])
     df.to_csv(csv_filename, index=False)
-    print(f"{Fore.GREEN}Success: Post counts saved to {csv_filename}{Style.RESET_ALL}")
+    print(f"\n{Fore.GREEN}Success: Post counts saved to {Fore.YELLOW}{csv_filename}{Style.RESET_ALL}")
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
