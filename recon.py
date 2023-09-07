f52dcb40d58d0df56c782e82e32915ce19da139e
sockysec <121141737+sockysec@users.noreply.github.com>
2023-09-08 10:00:44 +1200

    Update recon.py

--- recon.py
+++ recon.py
@@ -1,10 +1,11 @@
 import os
 import asyncio
 import details as ds
-from telethon import TelegramClient, errors
+from telethon import TelegramClient, errors, types
 import pandas as pd
-from tqdm import tqdm
 from colorama import Fore, Style
+import subprocess
+import sys
 
 # API details
 api_id = ds.apiID
@@ -12,7 +13,7 @@ api_hash = ds.apiHash
 phone = ds.number
 
 # Rate limiting settings
-REQUEST_DELAY = 2  # Delay in seconds between requests
+REQUEST_DELAY = 1  # Delay in seconds between requests
 
 async def count_user_posts(channel_name, target_user):
     async with TelegramClient(phone, api_id, api_hash) as client:
@@ -27,18 +28,20 @@ async def count_user_posts(channel_name, target_user):
             return post_count
 
         except (ValueError, errors.FloodWaitError) as ve:
-            print(f"\n{Fore.RED}Error: {ve}{Style.RESET_ALL}")
+            print(f"{Fore.RED}Error – lacking relevant permissions, or channel is not a chat group{Style.RESET_ALL}")
             return 0
         except Exception as e:
-            print(f"\n{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
+            print(f"{Fore.RED}Error – lacking relevant permissions, or channel is not a chat group{Style.RESET_ALL}")
             return 0
 
 async def main():
-    target_user = input(f"{Fore.CYAN}Please enter the target user's @username or User ID: {Style.RESET_ALL}")
-    target_list_filename = input(f"{Fore.CYAN}Please enter the filename of the target channel list (csv/txt): {Style.RESET_ALL}")
+    target_user = input(f"{Fore.CYAN}Please enter the target user's @username or User ID:{Style.RESET_ALL} ")
+    target_user = target_user.replace("@", "")  # Remove "@" symbol
+
+    target_list_filename = input(f"{Fore.CYAN}Please enter the filename of the target channel list (csv/txt):{Style.RESET_ALL} ")
 
     if not os.path.exists(target_list_filename):
-        print(f"\n{Fore.RED}Error: The file '{target_list_filename}' does not exist.{Style.RESET_ALL}")
+        print(f"{Fore.RED}Error: The file '{target_list_filename}' does not exist.{Style.RESET_ALL}")
         return
 
     target_channels = []
@@ -46,25 +49,38 @@ async def main():
         target_channels = [line.strip() for line in file]
 
     if not target_channels:
-        print(f"\n{Fore.YELLOW}Warning: No target channels found in the input file.{Style.RESET_ALL}")
+        print(f"{Fore.RED}Warning: No target channels found in the input file.{Style.RESET_ALL}")
         return
 
     post_counts = {}
 
+    output_directory = os.path.join("Collection", target_user)
+    os.makedirs(output_directory, exist_ok=True)  # Create the directory if it doesn't exist
+
     for target_channel in target_channels:
-        print(f"\n{Fore.CYAN}Checking activity in {Fore.YELLOW}{target_channel}...{Style.RESET_ALL}")
+        print()  # Print a blank line between channels
+        print(f"{Fore.CYAN}Checking activity in {Fore.YELLOW}{target_channel}...{Style.RESET_ALL}")
         post_count = await count_user_posts(target_channel, target_user)
         post_counts[target_channel] = post_count
 
         if post_count > 0:
-            print(f"\n{Fore.CYAN}{target_user} has {Fore.GREEN}{post_count}{Fore.CYAN} posts in {Fore.YELLOW}{target_channel}.{Style.RESET_ALL}")
+            print(f"{Fore.CYAN}{target_user} has {Fore.YELLOW}{post_count}{Fore.CYAN} posts in {Fore.YELLOW}{target_channel}.{Style.RESET_ALL}")
         else:
-            print(f"\n{Fore.CYAN}{target_user} has {Fore.RED}no posts{Fore.CYAN} in {Fore.YELLOW}{target_channel}.{Style.RESET_ALL}")
+            print(f"{Fore.CYAN}{target_user} has {Fore.YELLOW}no posts{Fore.CYAN} in {target_channel}.{Style.RESET_ALL}")
 
-    csv_filename = f'{target_user}_post_counts.csv'
+    csv_filename = os.path.join(output_directory, f'{target_user}_Activity.csv')
     df = pd.DataFrame(post_counts.items(), columns=['Channel', 'Post Count'])
     df.to_csv(csv_filename, index=False)
-    print(f"\n{Fore.GREEN}Success: Post counts saved to {Fore.YELLOW}{csv_filename}{Style.RESET_ALL}")
+    print() 
+    print(f"{Fore.GREEN}Success: Post counts saved to {csv_filename}{Style.RESET_ALL}")
+
+    # Ask the user if they want to scrape posts
+    print()  
+    scrape_choice = input(f"{Fore.CYAN}Do you want to scrape posts from target channels? (y/n): {Style.RESET_ALL} ").strip().lower()
+
+    if scrape_choice == "y":
+        # Launch usermultiscraper.py using subprocess and sys.executable
+        subprocess.run([sys.executable, "usermultiscraper.py", target_user])
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
